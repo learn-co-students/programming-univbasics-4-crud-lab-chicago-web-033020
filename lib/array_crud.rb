f28f94b6a03d134ab8bbfada67c59e1eca6c42a3
Yann Xiaoyan Yang <yannxyang233@gmail.com>
2020-03-10 23:58:55 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,40 @@
 def create_an_empty_array
-  
+  emptyAr=[]
 end
-
+##############################
 def create_an_array
-  
+  food=["pizza","coke","burger","orange"]
 end
-
+##############################
 def add_element_to_end_of_array(array, element)
-  
+array.push(element)
+#array<<element这个也行
 end
-
+##############################
 def add_element_to_start_of_array(array, element)
-  
+array.unshift(element)
 end
-
+##############################
 def remove_element_from_end_of_array(array)
-  
+array.pop
 end
-
+##############################
 def remove_element_from_start_of_array(array)
-  
+  array.shift
 end
-
+##############################
 def retrieve_element_from_index(array, index_number)
-  
+array[index_number]
 end
-
+##############################
 def retrieve_first_element_from_array(array)
-  
+array[0]
 end
-
+##############################
 def retrieve_last_element_from_array(array)
-  
+array[array.length-1]
 end
-
+##############################
 def update_element_from_index(array, index_number, element)
-
+array[index_number]=element
 end
